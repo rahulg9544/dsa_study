d8f118c9a8661ee0412c7d3a3f1e8144e1ac4cf2
Rahul G <1038@NXTGEN.BLR>
2023-01-04 11:52:40 +0530

    first commit

---
+++ php/Array.php
@@ -0,0 +1,18 @@
+<?php
+function fixArray(arr)
+{
+   sarr = arr.sort();
+   
+   
+   document.write(sarr);
+}
+
+ // Driver Code
+ 
+      arr = [19, 7, 0, 3, 18, 15, 12, 6, 1, 8,
+              11, 10, 9, 5, 13, 16, 2, 14, 17, 4];
+ 
+      // Function Call
+      fixArray(arr);
+
+?>
